403c7cbefd983b19f490384765a9fbbf9d2bb2f0
tomisin <tomjames156@gmail.com>
2024-04-07 10:10:54 +0100

    added function that randomly places items

--- Algorithms/TreasureHunt.java
+++ Algorithms/TreasureHunt.java
@@ -2,18 +2,24 @@ public class TreasureHunt{
 
     static void showMap(char[][] arr){
         // Show the outline for the map 
-        
+
         for(int x = 0; x < arr.length; x++){
             System.out.println("+---+---+---+---+---+");
             for(int y = 0; y < arr[0].length; y++){
-                if(arr[x][y] == 0){
-                    arr[x][y] = ' ';
-                }
 
                 if(y < 4){
-                    System.out.print("| " + arr[x][y] + " ");
+                    if(arr[x][y] == 0){
+                        System.out.print("|   ");
+                    }else{
+                        System.out.print("| " + arr[x][y] + " ");
+                    }                
                 }else{
-                    System.out.print("| " + arr[x][y] + " |");
+                    if(arr[x][y] == 0){
+                        System.out.print("|   |");
+                    }else{
+                        System.out.print("| " + arr[x][y] + " |");
+                    }
+                    
                 }
             }
 
@@ -22,12 +28,36 @@ public class TreasureHunt{
         System.out.println("+---+---+---+---+---+");
     }
 
+    static void placeItem(char item, char[][] arr){
+        // Function that randomly places a given item on the map
+
+        int row;
+        int col;
+
+        do{
+            row = (int) (Math.random() * arr.length);
+            col = (int) (Math.random() * arr[0].length);
+        }while(arr[row][col] != 0);
+
+        arr[row][col] = item;
+    }
+
     public static void main(String[] args){
         char[][] map = new char[5][5];
 
+        map[0][0] = 'P'; // place player at starting position
+        placeItem('T', map);  // randomly place treasure on the map
+
+        // randomly place 2 enemies on the map
+        placeItem('E', map);
+        placeItem('E', map);
+
+        // randomly place 3 obstacles on the map
+        placeItem('O', map);
+        placeItem('O', map);
+        placeItem('O', map);
         showMap(map);
 
-        map[0][0] = 'P';
-        showMap(map);
+        
     }
 }
