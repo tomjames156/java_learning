f647070cf9ebcae17696491adad1443e62316f6c
tomisin <tomjames156@gmail.com>
2024-04-07 11:29:49 +0100

    added moves functionality

--- Algorithms/TreasureHunt.java
+++ Algorithms/TreasureHunt.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TreasureHunt{
 
     static void showMap(char[][] arr){
@@ -42,10 +44,36 @@ public class TreasureHunt{
         arr[row][col] = item;
     }
 
+    static String promptUser(){
+        // Ask user to move and return user input
+
+        Scanner userMove = new Scanner(System.in);
+        System.out.print("\nMove (UP, DOWN, LEFT, RIGHT): \n");
+
+        String move = userMove.nextLine();
+
+        return move;
+    }
+
+    // static void checkEncounters(char[][] arr, String move){
+        
+
+    //     map[row][col] = 'P';
+    //     showMap(map);
+    // }
+
+
+
     public static void main(String[] args){
         char[][] map = new char[5][5];
+        int row = 0;
+        int col = 0;
+        String move;
+        int immunity = 0;
+        char[] helpfulItems = {'E', 'I'};
+        int foundTreasure = 0;
 
-        map[0][0] = 'P'; // place player at starting position
+        map[row][col] = 'P'; // place player at starting position
         placeItem('T', map);  // randomly place treasure on the map
 
         // randomly place 2 enemies on the map
@@ -56,8 +84,42 @@ public class TreasureHunt{
         placeItem('O', map);
         placeItem('O', map);
         placeItem('O', map);
-        showMap(map);
 
+        // randomly place a hidden item on the map
+        placeItem('H', map);
+        showMap(map);
         
+        int newRow = row;
+        int newCol = col;
+
+        while(foundTreasure != 1){
+
+            move = promptUser();
+
+            if(move.equals("RIGHT")){
+                newCol = col + 1;
+            }else if(move.equals("LEFT")){
+                newCol = col - 1;
+            }else if(move.equals("UP")){
+                newRow = row - 1;
+            }else if(move.equals("DOWN")){
+                newRow = row + 1;
+            }else if(move.equals("Q")){
+                break;
+            }else{
+                System.out.println("Invalid Move");
+            }
+
+            try{
+                map[newRow][newCol] = 'P';
+                map[row][col] = 0;
+                row = newRow;
+                col = newCol;
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("You're at an edge");
+            }
+            
+            showMap(map);
+        }
     }
 }
