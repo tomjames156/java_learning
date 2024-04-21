29e936e46472563c306984e4b8a5ad00273743e3
tomisin <tomjames156@gmail.com>
2024-04-21 16:00:44 +0100

    added place class and tourify package

---
+++ Praxis/tourify/Place.java
@@ -0,0 +1,29 @@
+public class Place{
+    private String name;
+    private float rating;
+    private String address;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(name){
+        this.name = name;
+    }
+
+    public float getRating(){
+        return this.rating;
+    }
+
+    public void setRating(rating){
+        this.name = rating;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setAddress(address){
+        this.address = address;
+    }
+}
