10ccc3364f6c0189303ccd6c44c3e7773d8704f1
tomisin <tomjames156@gmail.com>
2024-05-28 12:54:57 +0100

    added person class to models package

---
+++ Praxis/models/Person.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Person{
+    int age;
+    String name;
+    String from;
+
+    public Person(int _age, String _name, String _from){
+        age = _age;
+        name = _name;
+        from = _from;
+    }
+    
+}
