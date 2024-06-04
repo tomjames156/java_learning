c756b52d381bab2c4a79545f8993c946f0d2e684
tomisin <tomjames156@gmail.com>
2024-06-04 15:14:35 +0100

    added bad leap year method

--- ExamPractise/Programming/src/code_review/Date.java
+++ ExamPractise/Programming/src/code_review/Date.java
@@ -1,6 +1,22 @@
 package code_review;
 
 public class Date {
+
+
+    public static boolean smellyLeap(int y) {
+        String tmp = String.valueOf(y);
+        if (tmp.charAt(2) == '1' || tmp.charAt(2) == '3' || tmp.charAt(2) == 5 || tmp.charAt(2) == '7' || tmp.charAt(2) == '9') {
+        if (tmp.charAt(3)=='2'||tmp.charAt(3)=='6') return true; /*R1*/
+        else
+        return false; /*R2*/
+        }else{
+        if (tmp.charAt(2) == '0' && tmp.charAt(3) == '0') {
+        return false; /*R3*/
+        }
+        if (tmp.charAt(3)=='0'||tmp.charAt(3)=='4'||tmp.charAt(3)=='8')return true; /*R4*/
+        }
+        return false; /*R5*/
+       }
     
     public static int smellyDayOfYear(int month, int dayOfMonth, int year) {
         // Smelly code
